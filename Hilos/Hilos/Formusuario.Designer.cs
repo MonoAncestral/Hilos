0b6b224caebb7fec5de308d27804e5eb954bf8e4
teamzz111 <felipeflogxd@gmail.com>
2019-02-17 21:13:57 -0500

    Añadiendo cédula con eventos numéricos

--- Hilos/Hilos/Formusuario.Designer.cs
+++ Hilos/Hilos/Formusuario.Designer.cs
@@ -35,17 +35,36 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label2 = new System.Windows.Forms.Label();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.cedula = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button2
@@ -79,7 +98,6 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(256, 142);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(357, 75);
@@ -109,6 +127,7 @@
             // 
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
             this.tabControl1.Location = new System.Drawing.Point(322, 34);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
@@ -117,18 +136,27 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.BackColor = System.Drawing.Color.Black;
             this.tabPage1.Controls.Add(this.label3);
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.button2);
-            this.tabPage1.ForeColor = System.Drawing.Color.Coral;
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage1.Size = new System.Drawing.Size(843, 644);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(140, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(570, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "POR FAVOR SELECCIONE QUÉ CLASE DE USUARIO ES";
             // 
             // tabPage2
             // 
@@ -156,68 +184,227 @@
             // 
             // button3
             // 
+            this.button3.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button3.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button3.Image = global::Hilos.Properties.Resources._70964015_azul_verde_degradado_de_fondo_azul_ilustración_vectorial;
-            this.button3.Location = new System.Drawing.Point(259, 387);
+            this.button3.Location = new System.Drawing.Point(190, 445);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(325, 72);
+            this.button3.Size = new System.Drawing.Size(451, 89);
             this.button3.TabIndex = 6;
             this.button3.Text = "ASESORÍA DE SERVICIOS";
             this.button3.UseMnemonic = false;
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button4.Image = global::Hilos.Properties.Resources._70964015_azul_verde_degradado_de_fondo_azul_ilustración_vectorial;
-            this.button4.Location = new System.Drawing.Point(259, 309);
+            this.button4.Location = new System.Drawing.Point(190, 317);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(325, 72);
+            this.button4.Size = new System.Drawing.Size(451, 89);
             this.button4.TabIndex = 5;
             this.button4.Text = "CAJA";
             this.button4.UseMnemonic = false;
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             // 
             // button5
             // 
+            this.button5.BackColor = System.Drawing.SystemColors.MenuHighlight;
             this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button5.Font = new System.Drawing.Font("Arial Narrow", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.button5.Image = global::Hilos.Properties.Resources._70964015_azul_verde_degradado_de_fondo_azul_ilustración_vectorial;
-            this.button5.Location = new System.Drawing.Point(259, 231);
+            this.button5.Location = new System.Drawing.Point(190, 194);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(325, 72);
+            this.button5.Size = new System.Drawing.Size(451, 89);
             this.button5.TabIndex = 4;
             this.button5.Text = "ASESORIA COMERCIAL";
             this.button5.UseMnemonic = false;
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // label3
+            // tabPage3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(140, 228);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(570, 25);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "POR FAVOR SELECCIONE QUÉ CLASE DE USUARIO ES";
+            this.tabPage3.Controls.Add(this.panel1);
+            this.tabPage3.Controls.Add(this.cedula);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(843, 644);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button17);
+            this.panel1.Controls.Add(this.button16);
+            this.panel1.Controls.Add(this.button15);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.button14);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Location = new System.Drawing.Point(229, 314);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(374, 312);
+            this.panel1.TabIndex = 7;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(146, 243);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(59, 53);
+            this.button17.TabIndex = 11;
+            this.button17.Text = "0";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(56, 243);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(70, 53);
+            this.button16.TabIndex = 10;
+            this.button16.Text = "BORRAR";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(223, 243);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(70, 53);
+            this.button15.TabIndex = 9;
+            this.button15.Text = "ACEPTAR";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(223, 175);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(59, 53);
+            this.button12.TabIndex = 8;
+            this.button12.Text = "3";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(146, 175);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(59, 53);
+            this.button13.TabIndex = 7;
+            this.button13.Text = "2";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(67, 175);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(59, 53);
+            this.button14.TabIndex = 6;
+            this.button14.Text = "1";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(223, 100);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(59, 53);
+            this.button9.TabIndex = 5;
+            this.button9.Text = "6";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(146, 100);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(59, 53);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "5";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(67, 100);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(59, 53);
+            this.button11.TabIndex = 3;
+            this.button11.Text = "4";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(223, 29);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(59, 53);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "9";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(146, 29);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(59, 53);
+            this.button7.TabIndex = 1;
+            this.button7.Text = "8";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(67, 29);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(59, 53);
+            this.button6.TabIndex = 0;
+            this.button6.Text = "7";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // cedula
+            // 
+            this.cedula.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cedula.Location = new System.Drawing.Point(302, 238);
+            this.cedula.Name = "cedula";
+            this.cedula.Size = new System.Drawing.Size(208, 40);
+            this.cedula.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(270, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(277, 25);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "SIN PUNTOS NI ESPACIOS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(122, 105);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(585, 75);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "INGRESE SU CÉDULA";
             // 
             // Formusuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1230, 740);
             this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.panel2);
             this.Name = "Formusuario";
             this.Text = "Formusuario";
-            this.Load += new System.EventHandler(this.Formusuario_Load);
             this.panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.tabControl1.ResumeLayout(false);
@@ -225,6 +412,9 @@
             this.tabPage1.PerformLayout();
             this.tabPage2.ResumeLayout(false);
             this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -243,5 +433,22 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox cedula;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button17;
     }
 }
--- Hilos/Hilos/Formusuario.cs
+++ Hilos/Hilos/Formusuario.cs
@@ -12,9 +12,20 @@ namespace Hilos
 {
     public partial class Formusuario : Form
     {
+
         public Formusuario()
         {
             InitializeComponent();
+            button6.Click += new EventHandler(MyButtonClick);
+            button7.Click += new EventHandler(MyButtonClick);
+            button8.Click += new EventHandler(MyButtonClick);
+            button9.Click += new EventHandler(MyButtonClick);
+            button10.Click += new EventHandler(MyButtonClick);
+            button11.Click += new EventHandler(MyButtonClick);
+            button12.Click += new EventHandler(MyButtonClick);
+            button13.Click += new EventHandler(MyButtonClick);
+            button14.Click += new EventHandler(MyButtonClick);
+            button17.Click += new EventHandler(MyButtonClick);
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -22,9 +33,24 @@ namespace Hilos
             tabControl1.SelectedIndex = 1;
         }
 
-        private void Formusuario_Load(object sender, EventArgs e)
-        {
 
+        private void button5_Click(object sender, EventArgs e)
+        {
+            tabControl1.SelectedIndex = 2;
+        }
+
+        public void MyButtonClick(object sender, EventArgs e)
+        {
+            addText((Button)sender);
+        }
+        private void addText(Button button)
+        {
+            cedula.AppendText(button.Text);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
